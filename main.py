c25b75e8dad7a6564970b055bf1b742eb6820e6d
Viloul <ilya228.velizhev228@gmail.com>
2025-01-18 16:52:55 +0100

    update

--- main.py
+++ main.py
@@ -96,20 +96,28 @@
 #     # delete_user('test')
 #     # get_users()
 
-from datetime import time
+import datetime
+import time
 
 import telebot
 
-bot = telebot.TeleBot('7831552004:AAGsC6W5jLpB_FxaljAb8E-HqiYVUeIgznk')
+from func import add_user, create_table, log_message
 
+bot = telebot.TeleBot('7831552004:AAGsC6W5jLpB_FxaljAb8E-HqiYVUeIgznk')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    add_user(message.from_user.id, message.from_user.username)
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    log_message(message.from_user.id, message.text)
+    bot.send_message(message.chat.id, "Ваше сообщение сохранено в базе данных.")
 
 while True:
     try:
+        create_table()
         bot.polling(none_stop=True)
     except Exception as e:
         print(e)
